cd4ce7642fe7c34d59ae4a9cfa8cb05cc5bff657
Irha Ali <irhaali_97@hotmail.ca>
2023-04-08 22:37:51 -0400

    Python Input Feature

--- intro.py
+++ intro.py
@@ -32,3 +32,39 @@ print("Hello World")
 print(10)
 print(3.4)
 print(-100)
+
+'''
+Escape Sequences
+\\ - backlash - \
+\t -tab - (a tab)
+\n - newline - (a new line)
+\r - carriage return - (an archaic new line, windows uses these sometimes)
+\' - Single Quote - '
+\" - Double Quote - "
+\x48 - Hex Value 48 - H
+\u2184 - Unicode character 2184
+'''
+print("\u3456")
+print("Testing \n\t\"string")
+
+# Common coding standards
+#  Variable names should always start a alpha char
+# They can only use alphanumeric characters + a few special characters such as _
+
+# Assignment
+# Value to var
+# LHS <- RHS
+testing_one = 35
+
+# Input[PROMPT]
+number_of_viewers = input("How many viewers are in chat, right now?\n")
+print(number_of_viewers)
+
+# Collect information from the user so we can enroll them like a new student
+first_name = input("What is your first name?\n")
+last_name = input("What is your last name?\n")
+age = input("What is your age?\n")
+favourite_class = input("What is your favourite class?\n")
+
+print("Meet ", first_name, last_name, "who is ", age, "years old who enjoys", favourite_class, " class")
+
