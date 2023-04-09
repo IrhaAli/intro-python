360235e746ecc56ffe0968a21745ba4e1f134bf2
Irha Ali <irhaali_97@hotmail.ca>
2023-04-08 22:23:35 -0400

    Intro to python

---
+++ intro.py
@@ -0,0 +1,34 @@
+'''
+What is Python?
+- It is a beginner friendly interpretive language
+
+Data Types in Python
+- Numbers:
+  - Integers (Int)
+  - Float
+  - Complex
+'''
+# To declare a variable (name = value)
+num_of_watchers = 35
+num_of_messages = 10.7
+print(type(num_of_watchers))
+print(type(num_of_messages))
+
+# Boolean T/F
+kev_is_streaming = True
+hacker_named_404_is_present = True
+
+# Strings
+python_pro_web = "Is not what is in chat"
+
+# List (Array) & Dictionary (Map)
+
+
+# Print Statements 
+# print(WHAT YOU WANT TO PRINT)
+print("This is a string.")
+print("Printing strings need to be in quotes.")
+print("Hello World")
+print(10)
+print(3.4)
+print(-100)
